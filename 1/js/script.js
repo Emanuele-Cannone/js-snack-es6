a668964fec75c37cd4b4e83483dd2ca72941dd27
Emanuele <cannone.emanuele@gmail.com>
2021-01-19 13:36:05 +0100

    Init

---
+++ 1/js/script.js
@@ -0,0 +1,12 @@
+// 1) Utilizzando const e/o let. Inserire un numero, se è pari stampa il numero, 
+// se è dispari stampa il numero successivo.
+
+
+// let numeroUtente = parseInt(prompt('inserisci un numero'));
+
+// if (numeroUtente%2 == 0){
+//     console.log(numeroUtente);
+// } else {
+//     console.log(numeroUtente + 1);
+// }
+
