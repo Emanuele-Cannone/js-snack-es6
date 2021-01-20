f0daf6b08a48f993468737e02745c4bda21bff88
Emanuele <cannone.emanuele@gmail.com>
2021-01-20 18:03:31 +0100

    es2 con .filter

--- jsnack 2/js/script.js
+++ jsnack 2/js/script.js
@@ -11,14 +11,28 @@ const nuovoArray = [];
 const primoNumero = parseInt(prompt('inserisci un numero'))
 const secondoNumero = parseInt(prompt('inserisci un numero'))
 
+// esempio con forEach
 
-
-myArray.forEach((element, index) => {
+// myArray.forEach((element, index) => {
     
-    if (index >= primoNumero && index <= secondoNumero){
-        nuovoArray.push(element);
-    };
+//     if (index >= primoNumero && index <= secondoNumero){
+//         nuovoArray.push(element);
+//     };
 
- });
+//  });
 
- console.log(nuovoArray);
+//  console.log(nuovoArray);
+
+
+
+// esempio con .filter
+
+
+const ultimoArray = myArray.filter((element, index) => {
+
+    return index >= primoNumero && index <= secondoNumero
+
+});
+
+
+console.log(ultimoArray);
